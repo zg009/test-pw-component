c465753c226f742bf29078144b4fea3f38c946ae
Zach <zag0003@auburn.edu>
2023-05-18 21:53:41 -0500

    retargeted registration manager

--- src/StrongEmailPasswordAuthorizer.ts
+++ src/StrongEmailPasswordAuthorizer.ts
@@ -1,14 +1,9 @@
-import { AccountStore, CredentialsHandler, InteractionHandler, readJsonStream, Representation, InteractionHandlerInput, MethodNotAllowedHttpError } from "@solid/community-server";
-import assert from "assert";
+import {  RegistrationManager, RegistrationManagerArgs } from "@solid/community-server";
 
-export class StrongEmailPasswordAuthorizer extends InteractionHandler {
-    private readonly accountStore: AccountStore;
-    private readonly source: CredentialsHandler;
+export class StrongRegistrationManager extends RegistrationManager {
     
-    public constructor(accountStore: AccountStore, source: CredentialsHandler) {
-        super();
-        this.accountStore = accountStore;
-        this.source = source;
+    public constructor(args: RegistrationManagerArgs) {
+        super(args);
     }
 
     private validatePassword(password: string): boolean {
@@ -18,25 +13,6 @@ export class StrongEmailPasswordAuthorizer extends InteractionHandler {
         const digits = "1234567890";
         let hasOneDigit = password.split("").filter(elem => digits.includes(elem)).length > 0;
         return hasLength && hasOneDigit && hasOneSymbol;
-    }
-
-    private validateEmail(email: string): boolean {
-        const emailRegex = new RegExp(".{2,}@.{2,}\..{2,}");
-        return emailRegex.test(email);
-    }
-
-    public async handle({ operation }: InteractionHandlerInput): Promise<Representation> {
-        if (operation.method !== 'POST') {
-            throw new MethodNotAllowedHttpError([operation.method], "Only POST requests are allowed.");
-        }
-        const json = await readJsonStream(operation.body.data);
-        const { email, password } = json;
-        assert(typeof email === 'string' && this.validateEmail(email), 'Email does not satisfy requirements: something@whatever.end');
-        assert(typeof password === 'string' && this.validatePassword(password), 'Password does not satisfy requirements: 12 or more characters, with at least 1 digit and 1 symbol')
-        
-        const webId = await this.accountStore.authenticate(email, password);
-        delete json.password;
-        return this.source.handleSafe({ operation, body: { ...json, email, webId }});
     }
 
 }
